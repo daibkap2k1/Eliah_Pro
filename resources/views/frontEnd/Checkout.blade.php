c5d40cf37e5eb6a505b700de2490b14b29cc63f9
daibkap2k1 <59162066+daibkap2k1@users.noreply.github.com>
2020-05-29 16:23:16 +0700

    Project First

---
+++ resources/views/frontEnd/Checkout.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts/frontEnd/layoutFrontEnd')
+@section('titleWeb',"Eliah")
+@section('content')
+    <div class="wp-container">
+    <h1></h1>
+    <h3>test</h3>
+    </div>
+    <h4>test 2</h4>
+    <div class="casa">
+        sadasdasdasdasdas
+    </div>
+    <h3>test 20000</h3>
+@endsection
